d3ebfa707567ab96c91cc15b2a7cc6e0c9acce23
jourm <jourm91@gmail.com>
2020-02-20 17:58:03 +0000

    Add display of Github Repos

--- 05-moving_the_code_into_its_own_script/assets/js/github-information.js
+++ 05-moving_the_code_into_its_own_script/assets/js/github-information.js
@@ -14,6 +14,24 @@ function userInformationHTML(user) {
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`
 }            
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No Repos!</div>`
+    }
+    var listItemsHTML = repos.map(function(repo){
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                    </li>`;
+    });
+    return `<div class"clearfix repo-list">
+        <p>
+            <strong>Repo List:</strong>
+        </p>
+        <ul>
+            ${listItemsHTML.join("\n")}
+            </ul>
+    </div> `;
+}
 
 function fetchGitHubInformation(event){
 
@@ -28,11 +46,14 @@ function fetchGitHubInformation(event){
         </div>` );
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
-            $("#gh-user-data").html(userInformationHTML(userData));       
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));     
+            $("#gh-repo-data").html(repoInformationHTML(repoData));  
         }, function(errorResponse) {
             if (errorResponse.status === 404){
                 $("gh-user-data").html(`<h2>No info found for user ${username} </h2>`);
